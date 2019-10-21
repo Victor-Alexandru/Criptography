d2730608e35769a889ad94e64452e5f9dfb89d03
Victor-Alexandru <victor21cuciureanu@gmail.com>
2019-10-22 00:23:37 +0300

    nearly ready

--- Lab01/action.js
+++ Lab01/action.js
@@ -2,12 +2,55 @@ function main() {
     encription_function = {};
     decription_function = {};
 
+    isEncryptionGenerated = false;
+
+    validateLetter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 'space'];
 
     var randomProperty = function (obj) {
         // function to take random some keys from an object
         var keys = Object.keys(obj);
         return obj[keys[keys.length * Math.random() << 0]];
     };
+
+    function validateEncryptionFunction() {
+        let aValue = $("#a").val();
+        let bValue = $("#b").val();
+        let cValue = $("#c").val();
+        let dValue = $("#d").val();
+        let eValue = $("#e").val();
+        let fValue = $("#f").val();
+        let gValue = $("#g").val();
+        let hValue = $("#h").val();
+        let iValue = $("#i").val();
+        let jValue = $("#j").val();
+        let kValue = $("#k").val();
+        let lValue = $("#l").val();
+        let mValue = $("#m").val();
+        let nValue = $("#n").val();
+        let oValue = $("#o").val();
+        let pValue = $("#p").val();
+        let qValue = $("#q").val();
+        let rValue = $("#r").val();
+        let sValue = $("#s").val();
+        let tValue = $("#t").val();
+        let uValue = $("#u").val();
+        let vValue = $("#v").val();
+        let xValue = $("#x").val();
+        let yValue = $("#y").val();
+        let zValue = $("#z").val();
+        let wValue = $("#w").val();
+        let spaceValue = $("#space").val();
+        inputValues = [];
+        inputValues.push(aValue, bValue, cValue, dValue, eValue, fValue, gValue, hValue, iValue, jValue, kValue, lValue, mValue, nValue, oValue, pValue, qValue, rValue, spaceValue, sValue, tValue, uValue, vValue, wValue, xValue, yValue, zValue);
+        for (value of inputValues)
+        {
+            if(!validateLetter.includes(value))
+            {
+                alert("Invalid Encryption function input");
+                return
+            }
+        }
+    }
 
     function validateOnlySmallAndSpace(value) {
         // validate if the string contains only small letters and space
@@ -35,59 +78,17 @@ function main() {
 
     }
 
-    function generate_dict() {
-        //we generate the dictionary ,which is the encryption key for substitution cipher
-        for (let i = 97; i <= 122; i++) {
-            //at first we will have the keys matching the values
-            encription_function[String.fromCharCode(i)] = String.fromCharCode(i);
-        }
-
-        encription_function[' '] = ' ';
-
-    }
-
-    function shuffleFunction() {
-        //this function takes two random keys and swaps between their values
-        for (let i = 1; i < 100; i++) {
-            //taking the random keys
-            key1 = randomProperty(encription_function);
-            key2 = randomProperty(encription_function);
-            //swapping values
-            let aux = encription_function[key1];
-            encription_function[key1] = encription_function[key2];
-            encription_function[key2] = aux;
-
-        }
-
-        //calculating the decription function in which we invert the keys with the values
-        for (let i in encription_function) {
-            decription_function[encription_function[i].toUpperCase()] = i;
-        }
-        return decription_function;
-    }
-
-    function showTable() {
-        //we create a list with the dict and show it with jQuerry
-        list = '<ul>';
-        for (var i in encription_function) {
-            list += "<li> " + i.toString() + " ->" + encription_function[i].toString() + " </li>";
-        }
-        list += '</ul>';
-
-        listTwo = '<ul>';
-        for (var i in decription_function) {
-            listTwo += "<li> " + i.toString() + " ->" + decription_function[i].toString() + " </li>";
-        }
-        list += '</ul>';
-        listTwo += '</ul>';
-        $(".encryp_func").html(list + listTwo);
-    }
-
-
-    generate_dict();
+    $("#encryptionFunction").click(()=> {
+        //the callback which takes the value from the input ,validates it and encripts if its the case
+        validateEncryptionFunction();
+    });
 
 
     $("#encrypt-btn").click(function () {
+        if (!isEncryptionGenerated) {
+            alert("Encryption not generated");
+            return
+        }
         //the callback which takes the value from the input ,validates it and encripts if its the case
         let inputValue = $("#plainText").val();
         if (validateOnlySmallAndSpace(inputValue) === false) {
@@ -109,6 +110,10 @@ function main() {
     });
 
     $("#decrypt-btn").click(() => {
+        if (!isEncryptionGenerated) {
+            alert("Encryption not generated")
+            return
+        }
         //the callback which takes the value from the input ,validates it and decrypts if its the case ,also shows the table
         let inputValue = $("#cipherText").val();
         if (validateOnlyBigAndSpace(inputValue) === false) {
@@ -128,9 +133,6 @@ function main() {
             $(".res-iii").html(rez);
         }
     });
-
-    shuffleFunction();
-    showTable();
 
 
 }
